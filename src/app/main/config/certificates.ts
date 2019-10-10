5cf12603abca3dd27f16168bb98ec9fe7e53b834
Megaache Younes <megaache.younes@gmail.com>
2019-10-10 14:49:54 +0100

    updated config files

--- src/app/main/config/certificates.ts
+++ src/app/main/config/certificates.ts
@@ -1,27 +1,27 @@
 export const CERTIFICATES = [
     {
         title: 'Responsive web design',
-        link: 'https://www.freecodecamp.org/certification/megaacheyounes/responsive-web-design',
+        link: 'http' + 's://www.' + 'freecodecamp' + '.org/certification/megaacheyounes/responsive-web-design',
         issuer: 'FreeCodeCamp'
     }, {
         title: 'Javascript algorithms and data structures',
-        link: 'https://www.freecodecamp.org/certification/megaacheyounes/javascript-algorithms-and-data-structures',
+        link: 'htt' + 'ps:/' + '/www.freecodecamp.' + 'org/certification/megaacheyounes/javascript-algorithms-and-data-structures',
         issuer: 'FreeCodeCamp'
     }, {
         title: 'Apis and microservices',
-        link: 'https://www.freecodecamp.org/certification/megaacheyounes/apis-and-microservices',
+        link: 'htt' + 'ps://www.' + 'freecodecamp' + '.org/certification/megaacheyounes/apis-and-microservices',
         issuer: 'FreeCodeCamp'
     }, {
         title: 'Information security and quality assurance',
-        link: 'https://www.freecodecamp.org/certification/megaacheyounes/information-security-and-quality-assurance',
+        link: 'ht' + 'tps://www.' + 'freecodecamp' + '.org/certification/megaacheyounes/information-security-and-quality-assurance',
         issuer: 'FreeCodeCamp'
     }, {
         title: 'Java tutorial',
         issuer: 'SoloLearn',
-        link: 'https://www.sololearn.com/Certificate/1068-9450597/jpg'
+        link: 'ht' + 'tps://www.' + 'sololearn' + '.com/Certificate/1068-9450597/jpg'
     }, {
         title: 'PHP tutorial',
         issuer: 'SoloLearn',
-        link: 'https://www.sololearn.com/Certificate/1059-9450597/jpg'
+        link: 'htt' + 'ps://www.' + 'sololearn' + '.com/Certificate/1059-9450597/jpg'
     }
 ];
